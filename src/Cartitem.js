784d8db01c06f5129cc72475b7c8fbf453164d52
sour <dahiya.sourabh444@hmail.com>
2023-04-07 00:16:03 +0530

    ADDED STATE IN CARTITEM AND IMAGES IF BUTTONS

--- src/Cartitem.js
+++ src/Cartitem.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 class CartItem extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            price: 999,
+            title: 'Mobile Phone',
+            qty: 1,
+            img: ''
+        }
+    }
+
   render() {
+    const{price, title, qty} = this.state;
     return (
       <div className="cart-item">
         <div className="left-block">
@@ -9,12 +20,15 @@ class CartItem extends React.Component {
          
         </div>
         <div className="right-block">
-            <div style={{ fontSize: 25 }}>Phone</div>
-            <div style={{ color: '#777' }}>Rs 999</div>
-            <div style={{ color: '#777' }}>Qty: 1</div>
-            
+            <div style={{ fontSize: 25 }}>{title}</div>
+            <div style={{ color: '#777' }}>{price}</div>
+            <div style={{ color: '#777' }}>{qty}</div>
+
           <div className="cart-item-actions">
             {/* Buttons */}
+            <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/1828/1828906.png" />
+            <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/3524/3524388.png" />
+            <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/1214/1214428.png" />
           </div>
         </div>
       </div>
